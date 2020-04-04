a0a8784ce95fd4385c59298d8e1514ae012f02be
don616 <contatodonilo@gmail.com>
2020-04-04 01:14:17 -0300

    Adicionei mais palavras à lista de sorteio e organizeo em um bloco para ficar mais visível

--- jogoforca.py
+++ jogoforca.py
@@ -15,12 +15,21 @@ while True:
         continue
 
     # Palavras aleatórias que serão escolhidas para o jogo.
-    palavra = "arroz feijao batata macarrao tempero queijo caldo goiaba abacate tomate papel livro sorvete casa estudante biblioteca teclado frango estupido dicionario paralelepipedo leite vaca esperto gambiarra mouse monitor computador escola internet soldado sol ovo eu ela sim nao pao cao rio palavra tempestade aluguel predio telescopio milionario fone cloro bicicleta astronauta lua doutor medico mar cortina ceu montanha carro refrigerante prato rotina sentimento amor paz uniao sal pimenta espirito copo agua solidariedade farmacia sindrome medo nos"
+    palavra = """arroz feijao batata macarrao tempero queijo caldo goiaba abacate tomate
+                 papel livro sorvete casa estudante biblioteca teclado frango estupido 
+                 dicionario paralelepipedo leite vaca esperto gambiarra mouse monitor computador 
+                 escola internet soldado sol ovo eu ela sim nao pao cao rio palavra tempestade 
+                 aluguel predio telescopio milionario fone cloro bicicleta astronauta lua doutor 
+                 medico mar cortina ceu montanha carro refrigerante prato rotina sentimento amor 
+                 paz uniao sal pimenta espirito copo agua solidariedade farmacia sindrome medo nos 
+                 sempre convidado luz garrafa hipopotamo sociedade"""
+
     lista = palavra.split(" ")
     secreto = random.choice(lista)
     digitadas = []
     chances = 0
     quant_letras = len(secreto)
+
     # Seleciona quantas chances o usuario tem de acordo com a quantidade de letras da palavra escolhida.
     if quant_letras <= 4:
         chances = 3
@@ -28,10 +37,6 @@ while True:
         chances = 5
     else:
         chances = 7
-    """
-    print(secreto)
-    print(chances)
-    """
 
     print("-"*30)
     print()
@@ -61,7 +66,6 @@ while True:
                 print(f"Suas chances acabaram, você perdeu. A palavra era {secreto}.")
                 print()
                 break
-
 
         print()
         print(f"Você ainda tem {chances} chances ao total")
