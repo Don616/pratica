a307fb29b1a78d7b9f993af3e5958441d03e6e73
Donilo <donilo.pontes@vericode.com.br>
2022-02-01 10:28:02 -0300

    subindo primeiro script em powershell + modelo"

---
+++ powershell/hello.ps1
@@ -0,0 +1,24 @@
+﻿# =====================================================================
+#
+#    Comando para habilitar scripts:
+#    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+# 
+# =====================================================================
+# 
+#
+#    Nome do Programa: Hello World!
+#    Criado por: Don616
+#    Versão: 1.0
+#    Contato: https://github.com/Don616
+#    Descrição: Primeiro programa em PS1, para fins de teste.
+# 
+# 
+# =====================================================================
+# 
+
+#    Testando primeiro script PowerShell
+
+Write-Output "`nHello World!`n"
+$nome = Read-Host "Digite seu nome"
+Write-Output "`n==================`n"
+Write-Output "Bem vindo ao mundo PowerShell, $nome!"
---
+++ powershell/modelo.ps1
@@ -0,0 +1,17 @@
+﻿# =====================================================================
+#
+#    Comando para habilitar scripts:
+#    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
+# 
+# =====================================================================
+# 
+#
+#    Nome do Programa: 
+#    Criado por: Don616
+#    Versão: 1.0
+#    Contato: https://github.com/Don616
+#    Descrição: 
+# 
+# 
+# =====================================================================
+# 
